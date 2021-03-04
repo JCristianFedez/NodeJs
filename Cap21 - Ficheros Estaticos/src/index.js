67db5a85f70584e5171b94d92b7c829e88a64326
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2021-03-04 13:40:47 +0100

    Cap21 - Ficheros Estaticos

---
+++ Cap21 - Ficheros Estaticos/src/index.js
@@ -0,0 +1,47 @@
+import path from "path";
+import express from "express";
+import morgan from "morgan";
+import bodyParse from "body-parser";
+
+
+const app = express();
+app.disable("x-powered-by");
+
+app.set("env", "development");
+
+// Usando middleware morgan
+app.use(morgan("combined"));
+
+// Usando middlware bodyParse
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: false }));
+
+
+// Usando pug
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+
+// Rutas
+app.get("/", (req, res, next) => {
+    res.render("home", {
+        title: "Soy Home",
+        message: "Cap20 - Layouts"
+    });
+
+});
+
+app.get("/temario", (req, res, next) => {
+    res.render("temario", {
+        title: "Soy temario",
+        message: "Temario del curso de NodeJs"
+    });
+
+})
+
+// Usar ficheros estaticos del directorio public(En este ejemplo los css y js)
+app.use("/static" , express.static(path.join(__dirname, "public")));
+
+app.listen("9000", () => {
+    console.log("Servidor arrancado en http://localhost:9000");
+});
